6f081a780382a1d841cf79eea779bb553f7853ac
diego <dieguinho_loko@hotmail.com>
2016-04-11 14:34:14 -0300

    - Criado o template do projeto - Criado a Home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 11/04/16
+ * Time: 12:12
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Routing\Controller;
+
+class HomeController extends Controller
+{
+    public function visualizar()
+    {
+        $dados =
+        [
+            0 => [
+                "titulo" => "Man must explore, and this is exploration at its greatest",
+                "subtitulo" => "Problems look mighty small from 150 miles up",
+                "user" => "Usuario 1",
+                "created_at" => "2016-04-10"
+            ],
+            1 => [
+                "titulo" => "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.",
+                "subtitulo" => "",
+                "user" => "Usuario 2",
+                "created_at" => "2016-04-09"
+            ],
+            2 => [
+                "titulo" => "Science has not yet mastered prophecy",
+                "subtitulo" => "We predict too much for the next year and yet far too little for the next ten.",
+                "user" => "Usuario 1",
+                "created_at" => "2016-04-08"
+            ],
+            3 => [
+                "titulo" => "Failure is not an option",
+                "subtitulo" => "Many say exploration is part of our destiny, but it’s actually our duty to future generations.",
+                "user" => "Usuario 2",
+                "created_at" => "2016-04-07"
+            ],
+        ];
+
+        return view("home",compact('dados'));
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('template')
+@section('content')
+<div class="row">
+    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+        @foreach($dados as $dado)
+            <div class="post-preview">
+                <a href="#">
+                    <h2 class="post-title">
+                        {{ $dado['titulo'] }}
+                    </h2>
+                    <h3 class="post-subtitle">
+                        {{ $dado['subtitulo'] }}
+                    </h3>
+                </a>
+                <p class="post-meta">Posted by <a href="#">{{ $dado['user'] }}</a> on {{ strftime('%d %B, %Y', strtotime($dado['created_at'])) }}</p>
+            </div>
+            <hr>
+        @endforeach
+        <!-- Pager -->
+        <ul class="pager">
+            <li class="next">
+                <a href="#">Older Posts &rarr;</a>
+            </li>
+        </ul>
+    </div>
+</div>
+@stop
